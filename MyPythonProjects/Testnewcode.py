d137feebc3273d79984b172215dbeaa8346f9cb8
rf-clemco <rf@clemco.dk>
2025-06-24 08:21:38 +0200

    adjusted zigzag width

--- MyPythonProjects/Testnewcode.py
+++ MyPythonProjects/Testnewcode.py
@@ -14,11 +14,28 @@ with open("detected_rectangle.json") as f:
     rect_data = json.load(f)
 
 
+# --- Robot Initialization ---
+#R = abb.Robot(ip='127.0.0.1')
+R = abb.Robot(ip='192.168.125.1')
+
+
+R.set_joints([90, 0, 0, 0, 0, 0])  
+#R.set_external_axis([4000, 0, 0, 0, 0, 0])
+pose = R.get_cartesian()
+print("current pose of the robot, in millimeters :",pose)
+#
+pose0 = R.get_cartesian()
+print("Captured orientation:", pose0[1])
+
+external_axes = R.get_external_axis()
+print("External axis values:", external_axes)
 
 
 
+ext = R.get_external_axis()
+print("External axes:", ext)
 
-print("dist_from_left_cm:", rect_data["dist_from_left_cm"])
+#print("dist_from_left_cm:", rect_data["dist_from_left_cm"])
 
 #print("Loaded rectangle data:", rect_data)
 # ...rest of your code...
@@ -139,13 +156,6 @@ print("dist_from_left_cm:", rect_data["dist_from_left_cm"])
 #ext[0] += 1000.0
 #R.set_external_axis(ext)
 
-#R.set_joints([90, 90, 90, 0, 0, 0])  
-
-#pose = R.get_cartesian()
-#print("current pose of the robot, in millimeters :",pose)
-
-#pose0 = R.get_cartesian()
-#print("Captured orientation:", pose0[1])
 
 
 # To turn doMaterialOn HIGH:
